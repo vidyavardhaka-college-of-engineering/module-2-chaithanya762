afd75409c63671ac239a2aa5d1770cdfa496ec85
chaithanya762 <84765756+chaithanya762@users.noreply.github.com>
2021-06-14 21:18:59 +0530

    Update Program-5.c

--- Program-5.c
+++ Program-5.c
@@ -13,7 +13,7 @@ int main()
    printf("Enter number of elements in array and the array elements");
    scanf("%d", &n);
  
-  // printf("Enter %d elements\n", n);
+  printf("Enter %d elements\n", n);
  
    for (i = 0; i < n; i++)
    {
